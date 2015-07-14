9dbebd9461d67c1d2f1455417f46792e3bf66660
StanislavBozhilov <sisko.bg@gmail.com>
2015-07-14 15:18:32 +0300

    Commit

---
+++ Databases Apps (ORM Frameworks)/Homeworks/01_Entity-Framework-Intro/04_All-Employees-Native-SQL-query/EmployeeContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _01_Create_DbContext_for_the_SoftUni_database;
+
+namespace _04_All_Employees_Native_SQL_query
+{
+    public class EmployeeContext
+    {
+        public static void FindEmployeesWithProjects(int projectStartDateYear)
+        {
+            var softUniEntities = new SoftUniEntities();
+            var query = "SELECT [e].[FirstName]" +
+                        "FROM Employees [e]" +
+                        "JOIN EmployeesProjects [ep]" +
+                        "ON [ep].[EmployeeID] = [e].[EmployeeID]" +
+                        "JOIN Projects [p]" +
+                        "ON [p].[ProjectID] = [ep].[ProjectID]" +
+                        "WHERE YEAR([p].[StartDate]) = '{0}'" +
+                        "GROUP BY [e].[FirstName]" +
+                        "ORDER BY [e].[FirstName]";
+
+            var employeeFirstNames = softUniEntities.Database.SqlQuery<string>(String.Format(query, projectStartDateYear)).ToList();
+
+            foreach (var employeeFirstName in employeeFirstNames)
+            {
+                Console.WriteLine(employeeFirstName);
+            }
+        }
+    }
+}
